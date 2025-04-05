157adbecf93873f07cbc587c77d596e02bc1109c
Uri Shaked <uri@urishaked.com>
2025-04-05 22:29:15 +0300

    feat(wokwi-dht22/dht22-uno): initial test case

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wokwi Part Tests
+
+This repository contains a set of test projects for many Wokwi parts. The tests can be compiled 
+using platformio and then run using the Wokwi CLI.
---
+++ wokwi-dht22/dht22-uno/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHT22 test!"));
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again)
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+} 
