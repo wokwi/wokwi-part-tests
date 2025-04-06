5ceb856be848d7d1b07ec5773eb2bcb316ed5d1a
Uri Shaked <uri@urishaked.com>
2025-04-06 09:01:34 +0300

    feat(wokwi-dht22/dht22-esp32): add test

---
+++ wokwi-dht22/dht22-esp32/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("DHT22 test!");
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again)
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.printf("Humidity: %.2f%%  Temperature: %.2f°C\n", h, t);
+} 
